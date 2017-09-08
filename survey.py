f51514c7f7b91b9e88b5769735efe652386d0f4e
CLARKE KEATON E <kcla1801@education.mpls.k12.mn.us>
2017-09-08 15:04:45 -0500

    addinpythonfiles

---
+++ survey.py
@@ -0,0 +1,17 @@
+a = raw_input("What is your name?")
+
+print "Hello " + a
+
+b = raw_input("How many siblings do you have?")
+print "Wow " + b + " siblings"
+
+c = raw_input("What is your favorite color?")
+print "Lame!" 
+
+d = raw_input("Is this your favorite class?")
+print d + " Is that so?"
+
+e = raw_input("well... what is your least favorite class?")
+print e +" I'm telling!!!!"
+
+print "So " + a + "you have " + b + " siblings. Your favorite color and class is " + c + d + "and the class you absoultely hate is" + e
