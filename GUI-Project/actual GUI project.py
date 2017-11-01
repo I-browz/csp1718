2dfbc780f24e98721896ff0cc3e2cdab778dd89e
CLARKE KEATON E <kcla1801@education.mpls.k12.mn.us>
2017-11-01 14:59:35 -0500

    We fisnished A+++++

--- GUI-Project/actual GUI project.py
+++ GUI-Project/actual GUI project.py
@@ -3,30 +3,58 @@ Tkinter_tutorial.py demonstrates some of the standard widgets
 and the grid() geometry manager
 '''
 from Tkinter import *
+
+import PIL                # for most of the PIL routines
+from PIL import ImageTk   # this submodule is separated from PIL
+
+import matplotlib.pyplot as plt
+
+import os.path              
+
+
+
 root = Tk()
-'''
+
+
+
+
+
+
 # A canvas widget
 drawpad = Canvas(root, background='brown')
 drawpad.grid(row=0, column=1)
-item = drawpad.create_oval(10, 50, 100, 100, fill='green')
 
-# A checkbox widget
-box = Checkbutton(root, text="Check here.")
-box.grid(row=0, column=0)
-'''
+
+__dir__ = os.path.dirname(os.path.abspath(__file__))  
+filename = os.path.join(__dir__, 'DoraMiracle.jpg')
+
+img = PIL.Image.open(filename)
+
 
 '''
-# An editor widget
-editor = Text(width=80, height=10)
-editor.grid(row=2, column=1, rowspan=2, sticky=SE)
+im = Image.open('DoraMiracle.jpg')
+im.
 '''
+
+tkimg=PIL.ImageTk.PhotoImage(img)
+
+def stamp(event):
+    drawpad.create_image(event.x,event.y,image=tkimg)
+    print reduceSlider.get()
+drawpad.bind('<ButtonPress-1>', stamp)
+
+
+
+
+
+
 
 # A button. This widget is demonstrating using an event handler with 
 # the command argument
 times_pressed = 0
 def pressed():
     global times_pressed
-    times_pressed = 'You have just recived a treasure map, your goal is to decipher it.               '
+    times_pressed = 'To unlock the message click on the canvas of mystery above. You have just recived a treasure map, your goal is to decipher it.                     '
     editor.insert(END, times_pressed)
     editor.see(END)
 button = Button(root, text='Explorer Ready?', 
@@ -38,29 +66,15 @@ editor = Text(width=80, height=10)
 editor.grid(row=2, column=1, rowspan=2, sticky=SE)
 
 
-nextroot = Tk()
-
-
-
-def start(self):
-    times_pressed = 1
+times_pressed = 1
 def pressed():
     global times_pressed
-    times_pressed = 'How do you find the back of an egg? Click the one of the next two buttons                                '
+    times_pressed = '              How do you find the back of an egg? Click the one of the next two buttons                                '
     editor.insert(END, times_pressed)
     editor.see(END)
 button = Button(root, text='Solve this', 
                 command=pressed)
 button.grid(row=2, column=0)
-'''
-editor = Text(width=80, height=10)
-editor.grid(row=4, column=1, rowspan=2, sticky=SE)
-'''
-self.new_window = name_w
-
-
-
-nextroot = Tk()
 
 
 times_pressed = 1
@@ -72,10 +86,10 @@ def pressed():
 button = Button(root, text='Front', 
                 command=pressed)
 button.grid(row=3, column=0)
-def quit_window(self):
-    self.Q1()
-self.new_window.desroy()
-'''
+
+
+
+
 times_pressed = 1
 def pressed():
     global times_pressed
@@ -86,13 +100,6 @@ button = Button(root, text='Who cares',
                 command=pressed)
 button.grid(row=4, column=0)
 
-'''
-# Slider
-speed = IntVar()
-slider = Scale(root, from_=1, to=10, 
-               label='Speed', variable=speed)
-slider.grid(row=2, column=0)
-speed.get()
 '''
 # Radio buttons: you can only select one
 radio = [0]*4 # create a list
